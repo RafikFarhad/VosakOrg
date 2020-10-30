4ae93f102ccfc5e0faa8ed238fe2ef6ab5b7d9f8
RafikFarhad <rafikfarhad@gmail.com>
2020-10-30 23:35:39 +0600

    Swagger update

--- VosakOrgWeb/Controllers/Api/V1/AuthController.cs
+++ VosakOrgWeb/Controllers/Api/V1/AuthController.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace VosakOrg.Controllers.Api.V1
 {
-    [Route("auth")]
     [ApiVersion("1")]
+    [Produces("application/json")]
+    [Route("auth")]
     [ApiController]
     public class AuthController : ControllerBase
     {
         [HttpPost]
         [Route("login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult Login([FromBody] object request)
         {
             return Ok(request);
--- VosakOrgWeb/Controllers/Api/V1/PingController.cs
+++ VosakOrgWeb/Controllers/Api/V1/PingController.cs
@@ -1,18 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace VosakOrg.Controllers.V1
 {
     [ApiVersion("1")]
+    [Produces("application/json")]
     [ApiController]
     public class PingController : ControllerBase
     {
         [HttpGet]
         [Route("ping")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult Ping()
         {
             return Ok(new { message = "PONG", timestamp = DateTime.Now });
